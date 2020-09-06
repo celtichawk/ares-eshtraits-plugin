2ea4259590e580684cf0fe99d01a107407f15afd
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2020-09-06 01:19:34 -0400

    Updating Sheet Template
    Provided by Rucket82.

--- aresmush/plugins/eshtraits/commands/sheet_cmd.rb
+++ aresmush/plugins/eshtraits/commands/sheet_cmd.rb
@@ -1,32 +1,19 @@
 module AresMUSH
-    module ESHTraits
-        class SheetCmd
-            include CommandHandler
+	module ESHTraits
+		class SheetCmd
+			include CommandHandler
+			attr_accessor :target_name
 
-            attr_accessor :name, :powers, :skills, :advantages, :flaws 
+			def parse_args
+				self.target_name = cmd.args ? titlecase_arg(cmd.args) : enactor_name
+			end
 
-            def parse_args
-                self.name = cmd.args || enactor_name
-            end
-
-            def handle 
-                ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-                    template = BorderedDisplayTemplate.new model.powers, "#{model.name}'s Powers"
-                    client.emit template.render
-                end
-                ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-                    template = BorderedDisplayTemplate.new model.skills, "#{model.name}'s Skills"
-                    client.emit template.render
-                end
-                ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-                    template = BorderedDisplayTemplate.new model.advantages, "#{model.name}'s Advantages"
-                    client.emit template.render
-                end
-                ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-                    template = BorderedDisplayTemplate.new model.flaws, "#{model.name}'s Flaws"
-                    client.emit template.render
-                end               
-            end
-        end
-    end
+			def handle
+				ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+					template = SheetTemplate.new(model, client)
+					client.emit template.render
+				end
+			end
+		end
+	end
 end
